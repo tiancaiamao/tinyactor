854b9dbbd95cdf3da29b5486da45f2bfbbf90c30
tiancaiamao <tiancaiamao@gmail.com>
2026-09-25 03:09:50 +0800

    encoding: base64/url-safe/hex/percent codec (batch 2)
    static registration; decode-family -1 -> Err("invalid ...") lift in
    lib/encoding.ta; RFC3986 unreserved set for percent
    
    Co-authored-by: PGE task-encoding (eval PASS)

--- Makefile
+++ Makefile
@@ -111,7 +111,7 @@ else
 UNDEF_OK = -undefined dynamic_lookup
 endif
 
-SRC     = src/val.c src/vm.c src/builtin.c src/scheduler.c src/gc.c src/api.c src/net.c src/file.c src/os.c src/buf.c src/str.c src/num.c src/prof.c src/tavm.c
+SRC     = src/val.c src/vm.c src/builtin.c src/scheduler.c src/gc.c src/api.c src/net.c src/file.c src/os.c src/buf.c src/str.c src/num.c src/encoding.c src/prof.c src/tavm.c
 OBJ     = $(SRC:src/%.c=$(OBJ_DIR)/%.o)
 
 .PHONY: all clean test test-basic test-gc test-actor test-module test-compiler \
--- src/tavm.c
+++ src/tavm.c
@@ -25,6 +25,7 @@ extern void vm_register_os_module(VM *vm);
 extern void vm_register_buf_module(VM *vm);
 extern void vm_register_str_module(VM *vm);
 extern void vm_register_num_modules(VM *vm);
+extern void vm_register_encoding_module(VM *vm);
 extern void vm_register_vm_module(VM *vm);
 extern void vm_register_net_module(VM *vm);
 
@@ -96,6 +97,7 @@ int main(int argc, char **argv) {
     vm_register_buf_module(vm);
     vm_register_str_module(vm);
     vm_register_num_modules(vm);
+    vm_register_encoding_module(vm);
     vm_register_vm_module(vm);
 
     /* http is a dynamically-loaded C module (lib/http.dylib on macOS,
